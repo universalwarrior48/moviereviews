312a76ade95c18e1950c6359f212b3751eed2158
Sagar Pohekar <sagarpohekar@gmail.com>
2023-12-31 15:27:18 +0530

    Static Files, Production Env

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3l8$ih8&=dummwo!*b%mg88*u-64cj^099w(5y=ligt81rw*cj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -128,7 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = "static/"
-
+STATIC_ROOT = BASE_DIR / 'static'
 STATICFILES_DIRS = [
     BASE_DIR / 'moviereview/static/'
 ]
