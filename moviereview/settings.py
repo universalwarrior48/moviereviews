ef89e938fd0f3c9417cda640e1675af4629bae1e
Sagar Pohekar <sagarpohekar@gmail.com>
2024-01-01 12:35:13 +0530

    Hosts Config

--- moviereview/settings.py
+++ moviereview/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-3l8$ih8&=dummwo!*b%mg88*u-64cj^099w(5y=ligt81rw*cj"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
